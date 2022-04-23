f5de49685fb4b49eb11a388bc29dabe460fa0269
Karthik Bharadwaj <karthikb.cs19@rvce.edu.in>
2022-04-23 22:24:59 +0530

    stakeholders commit

---
+++ src/routes/stakeholders.svelte
@@ -0,0 +1,32 @@
+STAKEHOLDERS
+NFSU (National Forensics Science University)
+National Forensic Sciences University, an Institution of National Importance (erstwhile Gujarat Forensic Sciences University) isestablished by an act passed in year 2020 by Parliament of India, to facilitate and promote studies in conjunction with applied behavioural science studies, law, criminology and other allied areas and technology and other related fields, and to provide for matters connected therewith or incidental thereto. The University is established with an objective of fulfilling the acute shortage against increasing demand of forensic experts in the country and around the world. The University is one among very few Universities in Gujarat as well as in India to get NAAC accreditation with “A” grade from Ministry of Human Resource Management, Government of India. Considering the role, contribution and strategic importance of University in the field of forensic science, cyber security, homeland security and national security; Government of Gujarat has notified the University as “Institute of Strategic or Security related Interest” and a “Centre of Excellence”. The status of “Centre of Excellence for Research and Analysis of Narcotics Drugs and Psychotropic substances” is conferred by the Ministry of Home Affairs, Government of India to the University.
+
+A memorandum of understanding has been signed between NFSU and RVCE for setting up a “Centre for CCTV Research” at RVCE campus,to ensure better learning and research opportunities to the students and faculty members of both the institutes. Both the institutions can jointly organize expert talk/ seminar/workshops/summer internship/conference to improve industrial exposure and technical knowhow to students of both institutes and to help them in identifying real life challenges faced by the industries. Both institutions together can jointly take up some research projects, start-up activities and facilitate the consultancy services offered by Government/ Non- Government organizations.
+
+NFSU and RVCE can promote faculty and student exchange programs to create an outstanding academic and research environment.
+
+HAYAGRIVA SOFTWARE
+Hayagriva software is a product company based in Mumbai. Over the years, the company has created several software products,the current product focus being COM-SUR™, the world’s only CCTV video footage auditing, smart backup, and standardized intelligent incident reporting software. COM-SUR has gained tremendous traction and has won several awards.
+
+VIDEONETICS
+Videonetics, Unified Video Computing Platform(UVCP™) helps in making sense of surveillance, by providing an end-to-end solution for a wide range of applications. The platform is powered by our Artificial Intelligence and Deep Learning engine, which is trained on humongous data sets, making our solutions incredibly robust andsmart. All our products and solutions are integrated yet modular, ONVIF compliant, OS and hardware agnostic, scalable and interoperable.
+Videonetics has been ranked #1 Video Management Software provider in India, and among the top 5 in Asia (IHS/ Informa Tech Research). We remain driven by innovation, and committed to making the world a safer, smarter, happier place. Enabled with patented and award-winning AI and DL framework, Videonetics UVCP™ encompasses Intelligent VMS, Video Analytics, Intelligent Traffic Management System, Facial Recognition, Video Computing Platform as a Service (VCPaaS™) and Video Command Control Center.
+
+MISTRAL
+Mistral is an eminent system engineering and product design company focused on the embedded domain. Mistral is a technology design and system engineering company providing end-to-end embedded development services for product development and deployment. Mistral is among the leading product design and development companies in Bangalore, India.
+Mistral is an ISO 9001:2015, ISO 27001:2013, ISO 14001:2015, ISO 45001:2018 and AS9100D certified technology design and systems engineering company providing end-to-end solutions for product design and application deployment. Mistral is focused on three business domains viz.,Product Engineering Services, Aerospace & Defence and Homeland Security
+Mistral is an R&D centre recognized by the Department of Science and Technology, Govt. of India and certified by CEMILAC for design, testing and validation of defence electronics. Mistral is ISO/IEC 27001:2013 certified for its Information Security Management System (ISMS) that encapsulates Design and Development of Systems consisting of Embedded Software, Embedded Hardware and VLSI (FPGA).
+Mistral is one of the system integrator partner of Briefcam (an Israel based company) who have developed the Video synopsis software for video surveillance applications. The products supplied by them are Briefcam VS Enterprises and Briefcam VS Forensics.
+
+MIALO
+Headquartered in the Indian Silicon Valley, the city of Bengaluru, Mialo aspires to provide end to end software solutions and service needs of its clients globally - a true ONE STOP SHOP for all software solutions.
+•Unique and customized service offerings
+•High degree of flexibility and scalability
+•Adapt, be flexible, re-invent
+•Sustain and grow in a highly competitive landscape
+GateBOT developed by Mialo is an ANPR (Automatic number plate recognition) solution which is powered by real time edge processing end to end fully automated system backed by artificial intelligence to efficiently identify, monitor and authorise vehicles entering or exiting gated premises
+It detects entry and exit of vehicles and stores their details including license plate number vehicle type, in/out time etc in the database. It can be seamlessly integrated with existing/new CCTV cameras, boom barriers, automatic gates, traffic lights, other audio video alert systems, and QR reader to deliver a complete unmanned experience. It performs real time edge processing without costly servers, with optional internet connectivity.
+
+KRISHNA SOUND AND SECURE
+Krishna sound and secure located in Bengaluru, are leading suppliers and dealers of CCTV systems, audio systems, access control, home theatre systems and also provide access control solutions. They have installed 200 IP cameras in RVCE campus and they are marinating the entire CCTV network.
