e4ab0118b52a07b0e014f5be918376c9f80b841e
navnithb2001 <bharadwajnavnith5@gmail.com>
2022-04-23 23:17:08 +0530

    research commit2

--- src/routes/webinars.svelte
+++ src/routes/webinars.svelte
@@ -1,4 +1,4 @@
 <h2>Webinars</h2>
 
-The Center will be organising webinars once a month and the speakers are drawn from Academic Institutions, Govt. organisations and Industries. Topics for the webinars are mainly related to Video surveillance ,forensics and analytics domain. The first webinar was held on 22nd April, 2021 delivered by Dr. Surubhi Mathur, senior Assistant Professor, National Forensics Science University, Gujarat on “Forensic Analysis of CCTV/Video evidence.
-Link for the recorded talk:  <a href = https://youtu.be/wmuDFvlSp_w target="_blank">Link </a>
+<p>The Center will be organising webinars once a month and the speakers are drawn from Academic Institutions, Govt. organisations and Industries. Topics for the webinars are mainly related to Video surveillance ,forensics and analytics domain. The first webinar was held on 22nd April, 2021 delivered by Dr. Surubhi Mathur, senior Assistant Professor, National Forensics Science University, Gujarat on “Forensic Analysis of CCTV/Video evidence.
+    Link for the recorded talk:  <a href = https://youtu.be/wmuDFvlSp_w target="_blank">Link </a></p>
