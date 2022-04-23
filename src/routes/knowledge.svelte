331536abd7fb44604bf4e6fee59774c43bcbe8ea
navnithb2001 <bharadwajnavnith5@gmail.com>
2022-04-23 22:57:45 +0530

    knowledge commit1

---
+++ src/routes/knowledge.svelte
@@ -0,0 +1,35 @@
+<h2>CCTV</h2>
+
+<p>This page contains links to articles on CCTV and video surveillance systems extracted from the web.
+
+Happy reading!</p>
+
+<h3>Hyderabad model of CCTV surveillance</h3>
+
+Article link : The Hyderabad model of CCTV surveillance
+
+<h3>Railway Board has approved works for provision of Video Surveillance Systems covering 983 Railway stations under Nirbhaya funds</h3>
+
+Article link : works for provision of Video Surveillance
+
+<h3>Kerala Police to launch CIMS(central intrusion monitoring system), a first of its-kind surveillance system</h3>
+
+Article link : Kerala Police to launch CIMS
+
+<h3>Lucknow is counted amongst one of the "first CCTV city in India"</h3>
+
+Article link : First CCTV City in India
+
+<h3>Importance of performing a CCTV Audit: COM|SUR Gautam Goradia, Founder and CEO at Hayagriva Software Pvt.Ltd.</h3>
+
+Article link : Importance of performing a CCTV Audit
+
+<h2>WHITE PAPERS</h2>
+<ul>
+    <li>Delivering accelerated video analytics at the edge for AI Cities.</li>
+    <li>Video Analytics, specific by design.</li>
+    <li>Edge computing based surveillance framework for real time activity recognition</li>
+    <li>NEC Face recognition.</li>
+    <li>Intelligent storage for next generation video surveillance systems</li>
+    <li>New wave video surveillance capabilities</li>
+</ul>
