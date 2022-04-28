a5b0998d0d335ce75c641d709e316df33f33634b
Karthik Bharadwaj <karthikb.cs19@rvce.edu.in>
2022-04-28 18:59:55 +0530

    pushed eye

---
+++ src/routes/eye/consultancy.svelte
@@ -0,0 +1,3 @@
+<h1>Consultancy</h1>
+<p>The Center is capable of offering consultancy services to academic institutions, industries, corporates, police and other Sate and Govt organisations in providing the clients a turnkey solution for deploying video surveillance systems.</p>
+<p>The Center has several stakeholders on board with various domain expertise knowledge to provide services covering all types of cameras, recorders, storage ,networking, VMS , video analytics, video footage auditing ,video forensics, video synopsis, facial recognition, ANPR solutions and also to set up a full fledged integrated command and control center for it’s clients.</p>
---
+++ src/routes/eye/software_tools.svelte
@@ -0,0 +1,27 @@
+<h2>Videonetics Intellegent VMS 3.0</h2>
+<p>Videonetics Intelligent VMS 3.0 (Video Management Software) delivers unified, next generation user interface, military-grade security, rugged with unprecedented levels of high availability and a future ready solution, for any size of CCTV deployment. It analyses the attributes of servers, storage, network communication and other associated devices, with its AI algorithms in real time, and intelligently uses the capability of those devices to offer a fault-tolerant, fail-safe, responsive andrugged system for video management.</p> 
+<p>It handles cyber security threats with its multi-pronged security measures to ensure users data</p>
+<p>Privacy, security, and data integrity. Powered by an in-built video analytics framework ‘DeeperLook™’, Intelligent VMS 3.0 integrates various video analytics applications within a single unified video computing platform, hence both VMS and video analytics applications use common resources and objects - database, storage devices, data formats, thus reducing the overall IT infrastructure cost, besides ensuring data integrity, data interoperability, easy upgrades, and fast customisations</p>
+
+<h2>Comsur [Video Footage Auditing Software]</h2>
+<p>Com-Sur , the world’s only CCTV video footage auditing, smart standardised intelligent reporting software.</p>
+<p>Com-Sur is basically a software tool designed to carry out video footage auditing which works with all type of cameras and video management software systems/videoanalytics. It offers the following features which defines a clear work flow of CCTV video surveillance.</p>
+
+<p>Huge reduction of data size with automatic disaster recovery system</p>
+
+<p>he ability to run through (audit) hours of video footage in just few minutes</p>
+
+<p>Automatically creates incident reports in power point</p>
+
+<p>Delivery of business intelligence from the above reports.</p>
+
+<h2>BriefCam [Video Synopsis Software]</h2>
+<p>This Video Analytics software enables the quicker analysis of recorded and live video. It enables an user to get real-time alerts for defined events (like line-crossing, People count, Facial Match, etc) and, also lets an investigator review or search a recorded video
+based on attributes like gender, colour, clothing, direction, vehicle-types, size, speed etc. The information finally is then presented as a time compressed video with links for each event to the actual video stream.</p>
+
+<p>Briefcam has the sole product rights to Video Synopsis software and has two products on offer: Briefcam VS Enterprises and Briefcam VS Forensics, The Enterprise Version offers online and near real time creation of video summaries, while the Forensics version is a standalone(offline) packaging of the technology. It runs on a standard Windows OS platform.</p>
+<h2>GateBOT</h2>
+<p>It is an ANPR(Automatic Number Plate recognition) software solution which is powered, real time edge processing based end to end fully automated system backed by artificial intelligence to efficiently identify, monitor and authorise vehicles entering or exiting gated premises.</p>
+<p>It detects entry and exit of vehicles and stores their details including license plate number vehicle type, in/out time etc in the database.</p>
+<p>It can be seamlessly integrated with existing/new CCTV cameras, boom barriers, automatic gates, traffic lights, other audio video alert systems, and QR reader to deliver a complete unmanned experience. </p>
+<p>It performs real time edge processing without costly servers with optional internet connection. Runs on a standard Windows platform.</p>
