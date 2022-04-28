b01915852d2ac75d25b34873dca7195783c6f4ec
akhil <akhile76@gmail.com>
2022-04-28 19:37:54 +0530

    removed home image

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,8 +11,4 @@
 	/>
 </svg> -->
 
-<Team
-	name="abc"
-	designation="desg"
-	image="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-/>
+<Team name="abc" designation="desg" image="" />
