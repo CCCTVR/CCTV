804aa1548c11fe3d8a20178f1ceb683e1b97c260
navnithb2001 <bharadwajnavnith5@gmail.com>
2022-05-15 21:56:23 +0530

    knowledge link text css

--- src/routes/knowledge.svelte
+++ src/routes/knowledge.svelte
@@ -1,99 +1,113 @@
 <section class="bg-gray-100 min-h-screen">
 	<article class="prose-base lg:prose-2xl max-w-7xl mx-auto p-4">
-		<h1 class="text-orange-400">CCTV</h1>
+		<h1 class="">CCTV</h1>
 		<!-- make cards instead of table -->
 		<p>
 			This page contains links to articles on CCTV and video surveillance systems extracted from the
 			web. Happy reading!
 		</p>
 
-		<h3 class="text-orange-400">Hyderabad model of CCTV surveillance</h3>
+		<h3 class="">Hyderabad model of CCTV surveillance</h3>
 
 		<p>
 			Article link : <a
-				class="no-underline hover:underline"
+				class="text-blue-800 no-underline hover:underline"
 				href="https://www.livemint.com/news/india/the-hyderabad-model-of-cctv-surveillance-11604926158442.html"
 				>The Hyderabad model of CCTV surveillance</a
 			>
 		</p>
 
-		<h3 class="text-orange-400">
+		<h3 class="">
 			Railway Board has approved works for provision of Video Surveillance Systems covering 983
 			Railway stations under Nirbhaya funds
 		</h3>
 
 		<p>
 			Article link : <a
-				class="no-underline hover:underline"
+				class="text-blue-800 no-underline hover:underline"
 				href="https://pib.gov.in/PressReleasePage.aspx?PRID=1598772"
 				>works for provision of Video Surveillance</a
 			>
 		</p>
 
-		<h3 class="text-orange-400">
+		<h3 class=" ">
 			Kerala Police to launch CIMS(central intrusion monitoring system), a first of its-kind
 			surveillance system
 		</h3>
 
 		<p>
 			Article link : <a
+				class="text-blue-800 no-underline hover:underline"
 				href="https://www.theweek.in/news/india/2019/06/06/kerala-police-to-launch-cims-a-first-of-its-kind-surveillance-system.html"
 				>Kerala Police to launch CIMS</a
 			>
 		</p>
 
-		<h3 class="text-orange-400">
+		<h3 class=" ">
 			Lucknow is counted amongst one of the "first CCTV city in India"
 		</h3>
 
 		<p>
 			Article link : <a
+				class="text-blue-800 no-underline hover:underline"
 				href="https://www.knocksense.com/lucknow/read-along-find-out-why-lucknow-is-called-the-first-cctv-city-of-india"
 				>First CCTV City in India</a
 			>
 		</p>
 
-		<h3 class="text-orange-400">
+		<h3 class=" ">
 			Importance of performing a CCTV Audit: COM|SUR Gautam Goradia, Founder and CEO at Hayagriva
 			Software Pvt.Ltd.
 		</h3>
 
 		<p>
-			Article link : <a href="https://www.youtube.com/watch?v=bW-Bsp-y-ZQ"
+			Article link : <a 
+			class="text-blue-800 no-underline hover:underline"
+			href="https://www.youtube.com/watch?v=bW-Bsp-y-ZQ"
 				>Importance of performing a CCTV Audit</a
 			>
 		</p>
 
-		<h3 class="text-orange-400">WHITE PAPERS</h3>
+		<h3 class=" ">WHITE PAPERS</h3>
 		<ul>
 			<li>
 				<a
+				class="text-blue-800 no-underline hover:underline"
 					href="https://www.securityinfowatch.com/video-surveillance/document/12433313/whitepaper-delivering-accelerated-video-analytics-at-the-edge-for-ai-cities"
 					>Delivering accelerated video analytics at the edge for AI Cities.</a
 				>
 			</li>
 			<li>
-				<a href="https://securitynewsdesk.com/white-paper-video-analytics/"
+				<a 
+				class="text-blue-800 no-underline hover:underline"
+				href="https://securitynewsdesk.com/white-paper-video-analytics/"
 					>Video Analytics, specific by design.</a
 				>
 			</li>
 			<li>
-				<a href="https://www.sciencedirect.com/science/article/pii/S2405959521000540"
+				<a 
+				class="text-blue-800 no-underline hover:underline"
+				href="https://www.sciencedirect.com/science/article/pii/S2405959521000540"
 					>Edge computing based surveillance framework for real time activity recognition</a
 				>
 			</li>
 			<li>
-				<a href="https://www.nec.com/en/global/solutions/safety/pdf/NEC-FR_white-paper.pdf"
+				<a 
+				class="text-blue-800 no-underline hover:underline"
+				href="https://www.nec.com/en/global/solutions/safety/pdf/NEC-FR_white-paper.pdf"
 					>NEC Face recognition.</a
 				>
 			</li>
 			<li>
-				<a href="https://www.sourcesecurity.com/white-papers/surveillance-beyond-security.html"
+				<a 
+				class="text-blue-800 no-underline hover:underline"
+				href="https://www.sourcesecurity.com/white-papers/surveillance-beyond-security.html"
 					>Intelligent storage for next generation video surveillance systems</a
 				>
 			</li>
 			<li>
 				<a
+				class="text-blue-800 no-underline hover:underline"
 					href="https://www.sourcesecurity.com/insights/new-wave-video-surveillance-capabilities-co-12953-ga-co-14183-ga.1554455763.html"
 					>New wave video surveillance capabilities</a
 				>
