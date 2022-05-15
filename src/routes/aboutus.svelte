df94fd35ef335f3bb961cb62b5d82b3d687b49a0
navnithb2001 <bharadwajnavnith5@gmail.com>
2022-05-15 18:30:20 +0530

    about us

---
+++ src/routes/aboutus.svelte
@@ -0,0 +1,50 @@
+<h1>About Center for CCTV Research</h1>
+<br>
+<p>
+The Center for CCTV Research, located at Bengaluru’s prestigious RV College of Engineering campus has been created with the intention of being the catalyst to bridge the wide gap between the industry and creation & execution of humongous CCTV projects in the country. The Center aims to fulfill the gap of knowledge, practice, protocols, testing, experiments, certification, training, expertise etc. While being a one stop shop for all CCTV project requirements, it would be also the synthesis and the coordination center for all major CCTV players in the country and beyond. The creation and operations of this Center would be a clear manifestation of this dream.
+</p>
+
+<p>
+The research, business and the interregnum (the connect between the two) have never happened anywhere. They have always operated in silos and this has always remained the biggest pain point of both the customer and the industry. Unfortunately, the industry never realized its need. From the customer’s perspective, even the partners of the consortium do not gel and more often, the projects remain a patchy affair yielding no tangible results. The coming together of different business entities for a project is just crude distribution of work and a confident seamless consortium is required for this purpose.
+</p>
+<br>
+<h2>Vision</h2>
+<br>
+<p>
+    An Integrated research facility to bridge the gap in knowledge, practice, protocols, testing, experiments, training, certification and expertise in video surveillance with various industry partners
+</p>
+<p>
+    Providing a true multi-stake holder facility, being first of its kind in the country.
+</p>
+<br>
+<h2>Objectives</h2>
+<br>
+<ol>
+    <li>
+        To create standardizing Protocols for CCTV Surveillance Systems at the National level (National CCTV protocol) which will be responsible for properly guiding the stakeholders / user agencies.
+    </li>
+    <li>
+        To set up a research laboratory in an academic campus (RV College of Engineering) in order to carry out research and experiments related to validation of technical specifications of cameras, recorders, evaluation of Video Management Software (VMS), video surveillance and similar software.
+    </li>
+    <li>
+        To develop customized video analytics software and project management software that will manage all the activities of the Center.
+    </li>
+    <li>
+        To develop video command centre at RV College of Engineering at facilitate the centre for video analytics and related research
+    </li>
+    <li>
+        To create guidelines for preparing standard Request for Proposal (RFP) and Standard Operating Procedures (SOPs), compliances and thus create standardized RFP and SOP.
+    </li>
+    <li>
+        To explore the use of radios (point to point) in order to tremendously reduce the cost and time compared to wired networks.
+    </li>
+    <li>
+        To provide skill development and create awareness to various stakeholders to maintain integrity in project development and deployment.
+    </li>
+    <li>
+        To organize seminars and workshops to empower the Police officers, other law enforcement agencies, defence forces and government officials in order to disseminate necessary and sufficient domain knowledge related to CCTV deployments and maintenance and best practices.
+    </li>
+    <li>
+        To provide end to end seamless service with respect to CCTV deployments.
+    </li>
+</ol>
