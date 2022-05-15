c2d0172f7d7a67cb0e8ba81691b71e241a3c8a7c
navnithb2001 <bharadwajnavnith5@gmail.com>
2022-05-15 21:35:20 +0530

    about us

--- src/routes/test/tstFAQ.svelte
+++ src/routes/test/tstFAQ.svelte
@@ -1,7 +1,7 @@
 <script>
 	// on a tag apply text-indigo-500 underline
 	let QuestionsHedding = 'CCTV';
-	import { qlist } from '../../data/faq.js';
+	import { qlist1 } from '../../data/faq.js';
 </script>
 
 <section class="py-24 bg-white">
@@ -10,7 +10,7 @@
 			{QuestionsHedding}
 		</h2>
 		<div class="grid grid-cols-1 gap-0 text-gray-600 md:grid-cols-2 md:gap-x-16">
-			{#each qlist as { question, answer }}
+			{#each qlist1 as { question, answer }}
 				<div>
 					<h5 class="mt-10 text-xl mb-3 font-semibold text-gray-900">{question}</h5>
 					<p>
