79e6fdbe62af1f7852b4384054712509b20f2163
karthik-b-2001 <karthikb.cs19@rvce.edu.in>
2022-05-26 01:21:07 +0530

    minor-debuging

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -7,7 +7,7 @@
 <div
 	class=" h-full shadow-lg flex flex-nowrap border-solid border-blue-400 border-[1px] pt-2 hover:scale-105 transition-all duration-300 ease-linear rounded-2xl text-justify"
 >
-	<a href={Link} target="_blank">
+	<a href={Link}>
 		<div class="px-6 py-4">
 			<div class="font-bold text-2xl ">{Title}</div>
 			<p class="text-gray-700 text-lg mt-10">
