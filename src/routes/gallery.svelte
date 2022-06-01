2008fda5c5a9a2637acd102f587796c452b7b3b4
karthik-b-2001 <karthikb.cs19@rvce.edu.in>
2022-06-01 09:52:02 +0530

    gallery pics added

--- src/routes/gallery.svelte
+++ src/routes/gallery.svelte
@@ -3,12 +3,15 @@
 	import { GalleryImages } from '../data/data.js';
 	import { each } from 'svelte/internal';
 </script>
-<section class="min-h-screen" style="background-image: url('/src/assets/bg.png'); background-size: contain;">
+
+<section
+	class="min-h-screen"
+	style="background-image: url('/src/assets/bg.png'); background-size: contain;"
+>
 	<article class="prose-base lg:prose-2xl max-w-7xl mx-auto p-4 text-justify">
 		<h2 class="text-blue-800">Gallery</h2>
-	
 
-	<!-- <div class="pt-[1rem] flex">
+		<!-- <div class="pt-[1rem] flex">
 		<Carousel
 		  bind:this={carousel}
 		  autoplay
@@ -25,36 +28,59 @@
 		
 	</div> -->
 
-	<div class="carousel w-full">
-		<div id="slide1" class="carousel-item relative w-full">
-		  <img src="/src/assets/images/img1.jpg" class="w-full" />
-		  <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-			<a href="#slide4" class="btn btn-circle">❮</a> 
-			<a href="#slide2" class="btn btn-circle">❯</a>
-		  </div>
-		  
-		</div> 
-		<div id="slide2" class="carousel-item relative w-full">
-		  <img src="/src/assets/images/img2.jpg" class="w-full" />
-		  <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-			<a href="#slide1" class="btn btn-circle">❮</a> 
-			<a href="#slide3" class="btn btn-circle">❯</a>
-		  </div>
-		</div> 
-		<div id="slide3" class="carousel-item relative w-full">
-		  <img src="/src/assets/images/img3.jpg" class="w-full" />
-		  <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-			<a href="#slide2" class="btn btn-circle">❮</a> 
-			<a href="#slide4" class="btn btn-circle">❯</a>
-		  </div>
-		</div> 
-		<div id="slide4" class="carousel-item relative w-full">
-		  <img src="/src/assets/images/img9.jpg" class="w-full" />
-		  <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-			<a href="#slide3" class="btn btn-circle">❮</a> 
-			<a href="#slide1" class="btn btn-circle">❯</a>
-		  </div>
+		<div class="carousel w-full">
+			<div id="slide1" class="carousel-item relative w-full">
+				<img src="/src/assets/images/img1.jpg" class="w-full" />
+				<div
+					class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
+				>
+					<a href="#slide4" class="btn btn-circle">❮</a>
+					<a href="#slide2" class="btn btn-circle">❯</a>
+				</div>
+			</div>
+			<div id="slide2" class="carousel-item relative w-full">
+				<img src="/src/assets/images/img2.jpg" class="w-full" />
+				<div
+					class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
+				>
+					<a href="#slide1" class="btn btn-circle">❮</a>
+					<a href="#slide3" class="btn btn-circle">❯</a>
+				</div>
+			</div>
+			<div id="slide3" class="carousel-item relative w-full">
+				<img src="/src/assets/images/img3.jpg" class="w-full" />
+				<div
+					class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
+				>
+					<a href="#slide2" class="btn btn-circle">❮</a>
+					<a href="#slide4" class="btn btn-circle">❯</a>
+				</div>
+			</div>
+			<div id="slide4" class="carousel-item relative w-full">
+				<img src="/src/assets/images/img9.jpg" class="w-full" />
+				<div
+					class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
+				>
+					<a href="#slide3" class="btn btn-circle">❮</a>
+					<a href="#slide5" class="btn btn-circle">❯</a>
+				</div>
+			</div>
+			<div id="slide5" class="carousel-item relative w-full">
+				<img src="/src/assets/images/img8.jpg" class="w-full" />
+				<div
+					class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
+				>
+					<a href="#slide4" class="btn btn-circle">❮</a>
+					<a href="#slide6" class="btn btn-circle">❯</a>
+				</div>
+			</div>
+			<div id="slide6" class="carousel-item relative w-full">
+				<img src="/src/assets/images/img11.jpg" class="w-full" />
+				<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+				  <a href="#slide5" class="btn btn-circle">❮</a> 
+				  <a href="#slide1" class="btn btn-circle">❯</a>
+				</div>
+			  </div>
 		</div>
-	  </div>
-</article>
+	</article>
 </section>
